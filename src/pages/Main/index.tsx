ba571d42a5e3c939e2a0ccdefc000ce857c89fef
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2024-08-28 16:43:47 +0500

    styles

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -34,21 +34,20 @@ export default function Main() {
 
             {isStarted ? <Slider slides={SLIDES} showAnalyzer={isAnalyzeShowed} /> : null}
 
-            {!isStarted && (
+            {!isStarted ? (
                 <img className='mx-auto w-[50%]' src='/logo.png' alt='logo'/>
+            ) : (
+                <img className='fixed top-8 left-8 w-[250px] bg-white/50 rounded-3xl' src='/logo.png' alt='logo'/>
             )}
 
             <header className="text-center fixed bottom-0 left-0 right-0 flex justify-between items-center gap-3">
                 {isStarted
-                    ? (<>
-                            <img className='w-[250px] bg-white/50 my-8 ml-8 rounded-3xl' src='/logo.png' alt='logo'/>
-                            <div className='w-full h-full justify-end flex gap-4 p-8 transform translate-x-[55vw] hover:translate-x-0 inset-0 transition-transform duration-1000'>
-                                <Link to='/emotion'><Button>Посмотреть список анализов</Button></Link>
-                                <Button onClick={() => setIsAnalyzeShowed(!isAnalyzeShowed)}>
-                                    {isAnalyzeShowed ? 'Скрыть анализ' : 'Реалтайм анализ'}
-                                </Button>
-                            </div>
-                        </>
+                    ? (<div className='w-full h-full justify-end flex gap-4 p-8 transform translate-x-[55vw] hover:translate-x-0 inset-0 transition-transform duration-1000'>
+                           <Link to='/emotion'><Button>Посмотреть список анализов</Button></Link>
+                           <Button onClick={() => setIsAnalyzeShowed(!isAnalyzeShowed)}>
+                                {isAnalyzeShowed ? 'Скрыть анализ' : 'Реалтайм анализ'}
+                           </Button>
+                       </div>
                     )
                     : (<Button className='mx-auto m-8' onClick={() => setIsStarted(true)}>Начать просмотр слайдов</Button>)}
             </header>
